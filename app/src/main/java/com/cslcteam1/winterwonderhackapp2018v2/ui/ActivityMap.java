7f7b47b66ea32a709564dccd6b2ffdfbbdfa6a35
Alexander <36780824+thesilentwolf@users.noreply.github.com>
2018-02-24 15:04:09 -0500

    Change

--- app/src/main/java/com/cslcteam1/winterwonderhackapp2018v2/ui/ActivityMap.java
+++ app/src/main/java/com/cslcteam1/winterwonderhackapp2018v2/ui/ActivityMap.java
@@ -42,5 +42,4 @@ public class ActivityMap extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-
 }
