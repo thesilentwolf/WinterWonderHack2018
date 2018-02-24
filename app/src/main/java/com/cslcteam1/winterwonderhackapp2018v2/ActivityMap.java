a1cf76cbb225d311635d5ee612121df218354ad2
Shane <sttalbot@mtu.edu>
2018-02-24 04:18:09 -0500

    Added Button

--- app/src/main/java/com/cslcteam1/winterwonderhackapp2018v2/ActivityMap.java
+++ app/src/main/java/com/cslcteam1/winterwonderhackapp2018v2/ActivityMap.java
@@ -1,12 +1,15 @@
 package com.cslcteam1.winterwonderhackapp2018v2;
 
 import android.arch.persistence.room.Room;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.cslcteam1.winterwonderhackapp2018v2.db.DatabaseMain;
@@ -18,6 +21,21 @@ import java.util.logging.Logger;
 public class ActivityMap extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private Button but;
+
+    public void init(){
+        Button but = (Button)findViewById(R.id.button);
+
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent x = new Intent(ActivityMap.this, ActivityLocations.class);
+                startActivity(x);
+
+            }
+        });
+
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -50,6 +68,7 @@ public class ActivityMap extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        init();
     }
 
 
